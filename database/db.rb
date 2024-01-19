51c22f09bbb94a9d86c8fa45cff637315112baee
Unish <eunessshahithakuri@gmail.com>
2024-01-19 14:28:32 +0545

    Feat: Added gemfile

--- database/db.rb
+++ database/db.rb
@@ -1,20 +1,47 @@
+# frozen_string_literal: true
+
 require 'pg'
 
+# For Db connection
+class Db
+  def initialize
+    # Configuration database connection
+    @db_params = {
+      host: 'localhost',
+      port: 5432,
+      dbname: 'user_customer',
+      user: 'unesh',
+      password: ''
+    }
+  end
+
+  def connection
+    PG.connect(@db_params)
+  end
+end
+
+# db = Db.new
+# conn = db.connection
+
+# result = conn.exec('SELECT version()')
+
+# puts result[0]
+
 # Configuration database connection
-db_params = {
-  host: 'localhost',
-  port: 5432,
-  dbname: 'user_customer',
-  user: 'unesh',
-  password: ''
-}
+# db_params = {
+#   host: 'localhost',
+#   port: 5432,
+#   dbname: 'user_customer',
+#   user: 'unesh',
+#   password: ''
+# }
 
 # Established a connection to the database
-conn = PG.connect(db_params)
+# conn = PG.connect(db_params)
 
-# Execute a simple query
-result = conn.exec('SELECT version()')
-puts "PostgreSQL version: #{result[0]['version']}"
+# Execute a query to create table User and Customer
+# result = conn.exec('SELECT version()')
+# puts "PostgreSQL version: #{result[0]['version']}"
 
 # Close the connection when done
-conn.close
+# conn.close
